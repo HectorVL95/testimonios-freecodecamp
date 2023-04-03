363da67e2cbe185e866c19cbc8accb62602693bf
Hector Valenzuela <francisco1495@gmail.com>
2023-04-03 16:39:46 -0700

    1er commit

---
+++ src/components/Testimonio.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '../hojas-de-estilo/Testimonio.css'
+
+function Testimonio(props) {
+  return(
+    <div className="contenedor-testimonio">
+      <img className="imagen-testimonio"
+      src={require("../imagenes/testimonio-emma.png")} alt="Foto de Emma"/>
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">{props.nombre} {props.pais}</p>
+        <p className="cargo-testimonio">{props.cargo}{props.empresa}</p>
+        <p className="texto-testimonio">{props.testimonio}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonio;
